a86afada4fba9c581dd0c499a97335bada849eb4
robinmahto <robinmahto413@gmail.com>
2024-03-09 08:17:39 +0530

    add all content inside utils files

---
+++ utils/index.js
@@ -0,0 +1,86 @@
+export const navlist = [
+  "Web/App Builder",
+  "Influencer Marketing",
+  "SEO",
+  "Social Media Marketing",
+  "Content Writing",
+  "Graphic Design",
+  "Press Release",
+];
+
+export const heroContent = [
+  {
+    title: "We make projects so easy, everyone can do it",
+    description:
+      "We are having expertise into Web & App development, Graphic Design, Content Writing, Search Engine Optimisation (SEO), Social Media Management/Marketing (SMM), Press Release (PR). As having more than 10 years of expertise we can assure you for best quality results.",
+  },
+];
+
+export const mainContent = [
+  {
+    id: 1,
+    headingTitle1: "Web & App Development",
+    headingTitle2: "App for your business",
+    headingTitle3: "Your vision. Your app. We just do it.",
+    image: "/mainContent/web-and-app-development.jpg",
+    buttonName: "Try Now",
+  },
+  {
+    id: 2,
+    headingTitle1: "Influencer Marketing",
+    headingTitle2: "Amplified for your brand",
+    headingTitle3: "Elevate your brand with the power of influence",
+    image: "/mainContent/influencer-marketing.jpg",
+    buttonName: "Try Now",
+  },
+  {
+    id: 3,
+    headingTitle1: "Search Engine Optimisation",
+    headingTitle2: "Be visible on #Google",
+    headingTitle3: "Build brand position on search engine.",
+    image: "/mainContent/search-engine-optimization.jpg",
+    buttonName: "Try Now",
+  },
+  {
+    id: 4,
+    headingTitle1: "Graphic Design",
+    headingTitle2: "Draw Your Imagination",
+    headingTitle3:
+      "Get high quality design services tailored for your requirements.",
+    image: "/mainContent/graphic-design.jpg",
+    buttonName: "Try Now",
+  },
+  {
+    id: 5,
+    headingTitle1: "Social Media Management/Marketing",
+    headingTitle2: "Expand Social Reach",
+    headingTitle3:
+      "The cloud you need to run your software – without any hassle.",
+    image: "/mainContent/social-media-marketing.jpg",
+    buttonName: "Try Now",
+  },
+  {
+    id: 6,
+    headingTitle1: "Content Writing",
+    headingTitle2: "Best Quality Writing",
+    headingTitle3:
+      "Be it a cover letter or a researched case study, We have all in one place.",
+    image: "/mainContent/content-writing.jpg",
+    buttonName: "Try Now",
+  },
+];
+
+export const buyerContent = [
+  {
+    title: "BUILDER",
+    subTitle: "DO IT YOURSELF",
+    image: "/buyer/buyer-icons.jpg",
+  },
+];
+
+export const footerContent = [
+  {
+    title: "Copyright © 2024 Project Builder",
+    image: "/white-logo.png",
+  },
+];
