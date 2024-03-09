c64c5ac8cd418c2ad4f758d1cb43edc46591e7d6
robinmahto <robinmahto413@gmail.com>
2024-03-09 19:01:06 +0530

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,27 @@
+import { footerContent } from "@/utils";
+import Image from "next/image";
 
+footerContent;
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-black p-8">
+      {footerContent?.map((item, index) => {
+        return (
+          <div key={index} className="flex items-center justify-between">
+            <h3 className="text-white font-bold text-sm">{item.title}</h3>
+            <div>
+              <Image
+                src='/white-logo.png'
+                alt="footerLogo"
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Footer;
--- components/MainBody.jsx
+++ components/MainBody.jsx
@@ -15,11 +15,11 @@ const MainBody = () => {
                   : "justify-between px-10"
               }`}
             >
-              <div className="flex flex-col gap-3">
-                <h1 className="text-base font-bold mb-2 bg-yellow p-2">
+              <div className="flex flex-col  justify-center gap-2">
+                <h1 className="text-lg font-bold mb-2 bg-yellow p-2">
                   {item.headingTitle1}
                 </h1>
-                <h1 className="text-xl font-bold mb-2">{item.headingTitle2}</h1>
+                <h1 className="text-3xl font-bold mb-2">{item.headingTitle2}</h1>
                 <h1 className="text-sm mb-2">{item.headingTitle3}</h1>
                 <div>
                   <button className="px-5 py-2 border-2 border-grey-100 transform hover:scale-110 transition-transform duration-300 hover:border-purple-100 hover:text-purple-100">
